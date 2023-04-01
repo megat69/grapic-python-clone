9ecc65c676cb1917afbad0538ce4113f4730bd6e
megat69 <assassincraft218@gmail.com>
2023-04-01 16:46:30 +0200

    Added foreground color change

--- src/grapic/main_functions.py
+++ src/grapic/main_functions.py
@@ -79,3 +79,54 @@ def background_color(r: Union[int, float], g: Union[int, float], b: Union[int, f
 				f"r: {r} ({type(r)}), g: {g} ({type(g)}), b: {b} ({type(b)})"
 			)
 
+
+def color(r: Union[int, float], g: Union[int, float], b: Union[int, float]) -> None:
+	"""
+	Sets the foreground color of the window to the given r, g, b values.
+	All values should be integers between 0 and 255, or float values between 0 and 1.
+	:param r: The red component of the color. Int between 0 and 255 or Float between 0 and 1.
+	:param g: The green component of the color. Int between 0 and 255 or Float between 0 and 1.
+	:param b: The blue component of the color. Int between 0 and 255 or Float between 0 and 1.
+	"""
+	# If the values are integers with correct values
+	if all(isinstance(channel, int) for channel in (r, g, b)):
+		# Sets the color if the values are in the correct range
+		if all(0 <= channel <= 255 for channel in (r, g, b)):
+			Grapic._singleton.color = [r, g, b]
+
+		# Raises an exception if the values are not in the correct range
+		else:
+			raise ValueError(
+				f"Color could not be set, incorrect values received.\n"
+				f"Int values should be within range [0, 255].\n"
+				f"Got :\n"
+				f"- r: {r}\n"
+				f"- g: {g}\n"
+				f"- b: {b}\n"
+			)
+
+	# If the values are floats with correct values
+	elif all(isinstance(channel, float) for channel in (r, g, b)):
+		# Sets the background color to be the integer version of the parameters if the colors are in the correct range
+		if all(0 <= channel <= 1 for channel in (r, g, b)):
+			Grapic._singleton.color = [e * 255 for e in (r, g, b)]
+
+		# Raises an exception if the values are not in the correct range
+		else:
+			raise ValueError(
+				f"Color could not be set, incorrect values received.\n"
+				f"Float values should be within range [0, 1].\n"
+				f"Got :\n"
+				f"- r: {r}\n"
+				f"- g: {g}\n"
+				f"- b: {b}\n"
+			)
+
+	# If the values are incorrect
+	else:
+		# Here raises an exception if not all values are ints or floats
+		if not (all(isinstance(channel, int) for channel in (r, g, b)) and all(isinstance(channel, int) for channel in (r, g, b))):
+			raise TypeError(
+				f"Color could not be set, incorrect types received.\n"
+				f"r: {r} ({type(r)}), g: {g} ({type(g)}), b: {b} ({type(b)})"
+			)
